d86a2685456ebaf624c1afa2cf403006c9245d71
Helen Virtanen <helenvirtanen28@gmail.com>
2025-11-14 16:47:23 +0300

    feat(ArticleParamsForm): add form heading

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -43,6 +43,7 @@ export const ArticleParamsForm = () => {
 				ref={sidebarRef}
 				className={clsx(styles.container, isOpen && styles.container_open)}>
 				<form className={styles.form}>
+					<h1 className={styles.heading}>Задайте параметры</h1>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
